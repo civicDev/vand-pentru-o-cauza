b8988b42332d4f4e89a3fe22cddd6bce4b57ec05
mnanu <mircea.nanu@gmail.com>
2016-11-21 18:26:27 +0200

    traduceri si upload de extensie fara ftp

---
+++ website/system/storage/modification/catalog/language/ro-ro/account/account.php
@@ -0,0 +1,26 @@
+<?php
+// Heading
+$_['heading_title']      = 'Contul meu';
+
+// Text
+$_['text_seller']        = 'Donor';
+$_['text_sellerprofile'] = 'Profil donor';
+$_['text_bankaccount']   = 'Your Bank Account';
+$_['text_sellerorder']   = 'Comenzi pentru produsele tale';
+$_['text_sellerproduct'] = 'Produsele tale';
+$_['text_account']       = 'Cont';
+$_['text_my_account']    = 'Contul meu';
+$_['text_my_orders']     = 'Comenzi';
+$_['text_my_newsletter'] = 'Newsletter';
+$_['text_edit']          = 'Edit your account information';
+$_['text_password']      = 'Change your password';
+$_['text_address']       = 'Modify your address book entries';
+$_['text_wishlist']      = 'Modify your wish list';
+$_['text_order']         = 'View your order history';
+$_['text_download']      = 'Downloads';
+$_['text_reward']        = 'Your Reward Points';
+$_['text_return']        = 'View your return requests';
+$_['text_transaction']   = 'Your Transactions';
+$_['text_newsletter']    = 'Subscribe / unsubscribe to newsletter';
+$_['text_recurring']     = 'Recurring payments';
+$_['text_transactions']  = 'Transactions';
---
+++ website/system/storage/modification/catalog/language/ro-ro/common/header.php
@@ -0,0 +1,20 @@
+<?php
+	// Text
+	$_['text_seller_account']     = 'Contul meu (donor)';
+	$_['text_seller_order']       = 'Seller Orders';
+	$_['text_seller_product']     = 'Produsele mele';
+	$_['text_seller_bankaccount'] = 'Conturi bancare';
+	$_['text_home']          	  = 'Acasa';
+	$_['text_wishlist']      	  = 'Lista dorinte (%s)';
+	$_['text_shopping_cart'] 	  = 'Shopping Cart';
+	$_['text_category']      	  = 'Categories';
+	$_['text_account']       	  = 'Contul meu';
+	$_['text_register']      	  = 'Inregistreaza-te';
+	$_['text_login']         	  = 'Login';
+	$_['text_order']         	  = 'Order History';
+	$_['text_transaction']   	  = 'Transactions';
+	$_['text_download']      	  = 'Downloads';
+	$_['text_logout']        	  = 'Logout';
+	$_['text_checkout']      	  = 'Checkout';
+	$_['text_search']        	  = 'Search';
+	$_['text_all']           	  = 'Show All';
